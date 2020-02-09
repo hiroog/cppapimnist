ec3fb18c5f31ac41a4426e21bd3768842108711e
Hiroyuki Ogasawara <hiroog@flatlib.jp>
2020-02-10 00:05:03 +0900

    mxnet

---
+++ python/mxnet_keras.py
@@ -0,0 +1,71 @@
+# 2020/02/09 Hiroyuki Ogasawara
+# vim:ts=4 sw=4 et:
+
+import os
+os.environ['KERAS_BACKEND']='mxnet'
+
+import keras
+from keras import models
+from keras import layers as l
+import numpy as np
+import mnist_loader
+
+
+# channels_first + functional API + mse
+
+def test_train():
+    x0= l.Input( shape=(1,28,28) )
+    x= l.Conv2D( 16, (5,5), kernel_initializer='he_normal', activation='relu' )( x0 )
+    x= l.MaxPooling2D( pool_size=(2,2) )( x )
+    x= l.Dropout( 0.25 )( x )
+    x= l.Conv2D( 32, (5,5), kernel_initializer='he_normal', activation='relu' )( x )
+    x= l.MaxPooling2D( pool_size=(2,2) )( x )
+    x= l.Dropout( 0.25 )( x )
+    x= l.Flatten()( x )
+    x= l.Dense( 128, kernel_initializer='he_normal', activation='relu' )( x )
+    x= l.Dropout( 0.5 )( x )
+    x= l.Dense( 64, kernel_initializer='he_normal', activation='relu' )( x )
+    x= l.Dropout( 0.5 )( x )
+    x= l.Dense( 10, kernel_initializer='he_normal' )( x )
+    model= models.Model( inputs=x0, outputs=x )
+    model.compile( loss='mse', metrics=['accuracy'], optimizer=keras.optimizers.Adam( lr=0.01 ) )
+    model.summary()
+
+    loader= mnist_loader.MNistLoader( '../mnist' )
+    (x_train,y_train),(x_test,y_test)= loader.getAll()
+
+    model.fit( x_train, y_train, batch_size=32, epochs=2, verbose=1 )
+    model.save( 'python_mnist_mxnet_keras.h5' )
+
+
+def test_predict():
+    BATCH_SIZE=64
+    model= models.load_model( 'python_mnist_mxnet_keras.h5' )
+
+    loader= mnist_loader.MNistLoader( '../mnist' )
+    (x_train,y_train),(x_test,y_test)= loader.getAll()
+
+    loop_count= len(x_test) // BATCH_SIZE
+
+    score= 0
+    for i in range(loop_count):
+        rand_index= np.random.randint( len(x_test), size=BATCH_SIZE )
+        x_data= x_test[rand_index]
+        y_data= y_test[rand_index]
+        result= model.predict( x_data, batch_size=BATCH_SIZE, verbose=0 )
+        for ba,bb in zip(result,y_data):
+            ra= np.argmax( ba )
+            rb= np.argmax( bb )
+            if ra == rb:
+                score+= 1
+    print( score * 100.0 / (loop_count * BATCH_SIZE), '%' )
+
+
+def main():
+    test_train()
+    test_predict()
+
+if __name__=='__main__':
+    main()
+
+
---
+++ python/mxnet_python.py
@@ -0,0 +1,94 @@
+# 2020/02/09 Hiroyuki Ogasawara
+# vim:ts=4 sw=4 et:
+
+import mxnet
+from mxnet.gluon import nn as l
+from mxnet import autograd
+import numpy as np
+import mnist_loader
+
+
+def def_model():
+    model= l.Sequential()
+    model.add( l.Conv2D( 16, kernel_size=(5,5), activation='relu' ) )
+    model.add( l.MaxPool2D( pool_size=(2,2) ) )
+    model.add( l.Dropout( 0.25 ) )
+    model.add( l.Conv2D( 32, kernel_size=(5,5), activation='relu' ) )
+    model.add( l.MaxPool2D( pool_size=(2,2) ) )
+    model.add( l.Dropout( 0.25 ) )
+    model.add( l.Flatten() )
+    model.add( l.Dense( 128, activation='relu' ) )
+    model.add( l.Dropout( 0.5 ) )
+    model.add( l.Dense( 64, activation='relu' ) )
+    model.add( l.Dropout( 0.5 ) )
+    model.add( l.Dense( 10 ) )
+    return  model
+
+
+def test_train():
+    loader= mnist_loader.MNistLoader( '../mnist' )
+    (x_train,y_train),(x_test,y_test)= loader.getAll()
+
+    EPOCH=2
+    BATCH_SIZE=32
+    loop_count= len(x_train) // BATCH_SIZE
+
+    device= mxnet.gpu() if mxnet.context.num_gpus() else mxnet.cpu()
+
+    model= def_model()
+    model.collect_params().initialize( mxnet.init.Xavier(), ctx=device )
+
+    optimizer= mxnet.gluon.Trainer( model.collect_params(), 'adam', { 'learning_rate': 0.001 } )
+    loss_func= mxnet.gluon.loss.HuberLoss()
+
+    for ei in range(EPOCH):
+        total_loss= 0.0
+        for di in range(loop_count):
+            rindex= np.random.randint( len(x_train), size=BATCH_SIZE )
+            x_data= mxnet.nd.array( x_train[ rindex ] ).as_in_context( device )
+            y_data= mxnet.nd.array( y_train[ rindex ] ).as_in_context( device )
+            with autograd.record():
+                outputs= model( x_data )
+                loss= loss_func( outputs, y_data )
+                loss.backward()
+            optimizer.step( x_data.shape[0] )
+            total_loss+= loss[0].asscalar()
+        print( ei, 'loss=', total_loss / loop_count )
+
+    model.save_parameters( 'python_mnist_mxnet_python.params' )
+
+
+def test_predict():
+    loader= mnist_loader.MNistLoader( '../mnist' )
+    (x_train,y_train),(x_test,y_test)= loader.getAll()
+
+    BATCH_SIZE=64
+    loop_count= len(x_test) // BATCH_SIZE
+
+    device= mxnet.gpu() if mxnet.context.num_gpus() else mxnet.cpu()
+
+    model= def_model()
+    model.load_parameters( 'python_mnist_mxnet_python.params', ctx=device )
+
+    score= 0
+    for i in range(loop_count):
+        rand_index= np.random.randint( len(x_test), size=BATCH_SIZE )
+        x_data= mxnet.nd.array( x_test[rand_index] ).as_in_context( device )
+        y_data= y_test[rand_index]
+        result= model( x_data ).asnumpy()
+        for ba,bb in zip(result,y_data):
+            ra= np.argmax( ba )
+            rb= np.argmax( bb )
+            if ra == rb:
+                score+= 1
+    print( score * 100.0 / (loop_count * BATCH_SIZE), '%' )
+
+
+def main():
+    test_train()
+    test_predict()
+
+if __name__=='__main__':
+    main()
+
+
